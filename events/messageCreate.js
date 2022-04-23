4493a424bb471b2955ebe26ea3a07d1ad1857816
Deadr3amer <87982019+Deadr3amer@users.noreply.github.com>
2022-04-23 23:49:18 +0200

    Chat filter
    Allows only messages with .qach extension

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,25 @@
 const client = require("../index");
+const { channelToWatch, allowedFileExtension } = require("../config.json");
 
 client.on("messageCreate", async (message) => {
+    if(message.author.bot) return null;
+
+    //Checks if the message is in the watched channel
+    if(message.channel.id === channelToWatch) {
+        //If no attachments, delete the message
+        if(!message.attachments.first()) {
+            await message.delete();
+        }
+        //If an attachment doesn't have the allowed extension
+        else if(message.attachments.first().name.split(".").pop() !== allowedFileExtension) {
+            await message.delete();
+        }
+        //If an attachment meets all requirements to be posted, do nothing
+        else if(message.attachments.first().contentType && message.attachments.first().name.split(".").pop() === allowedFileExtension) {
+            return null;
+        }
+    }
+    //Command Handler
     if (
         message.author.bot ||
         !message.guild ||
@@ -8,7 +27,7 @@ client.on("messageCreate", async (message) => {
     )
         return;
 
-    const [cmd, ...args] = message.content
+    const [ cmd, ...args ] = message.content
         .slice(client.config.prefix.length)
         .trim()
         .split(/ +/g);
